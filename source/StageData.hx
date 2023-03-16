0c2b1c6c0407957b450b4828f28930e6ba46be66
SINIX_SKR <116418436+SINIXgo@users.noreply.github.com>
2023-03-15 23:18:32 -0300

    Add files via upload

--- source/StageData.hx
+++ source/StageData.hx
@@ -3,7 +3,7 @@ package;
 #if MODS_ALLOWED
 import sys.io.File;
 import sys.FileSystem;
-#elseif android
+#else
 import openfl.utils.Assets;
 #end
 import haxe.Json;
@@ -72,7 +72,7 @@ class StageData {
 		var rawJson:String = null;
 		var path:String = Paths.getPreloadPath('stages/' + stage + '.json');
 
-		#if desktop 
+		#if MODS_ALLOWED
 		var modPath:String = Paths.modFolders('stages/' + stage + '.json');
 		if(FileSystem.exists(modPath)) {
 			rawJson = File.getContent(modPath);
@@ -80,16 +80,14 @@ class StageData {
 			rawJson = File.getContent(path);
 		}
 		#else
-		if(Assets.exists(path)
-			
-		
+		if(Assets.exists(path)) {
+			rawJson = Assets.getText(path);
+		}
 		#end
-		
-		
-			
-		
-		
-	
-
-)
-                     }
+		else
+		{
+			return null;
+		}
+		return cast Json.parse(rawJson);
+	}
+}
